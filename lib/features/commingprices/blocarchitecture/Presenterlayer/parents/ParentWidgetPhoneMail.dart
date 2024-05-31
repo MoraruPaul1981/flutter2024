1e50e0b0a5833900d13302dd9dfb5e3c5823338a
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-05-31 14:56:02 +0300

    flutter may

--- lib/features/commingprices/blocarchitecture/Presenterlayer/parents/ParentWidgetPhoneMail.dart
+++ lib/features/commingprices/blocarchitecture/Presenterlayer/parents/ParentWidgetPhoneMail.dart
@@ -24,36 +24,45 @@ class ParentWidgetPhoneMail extends State<WidgetStateful>  {
   Widget build(BuildContext context) {
 
     return   Scaffold(
-      backgroundColor:     Colors.red,
+      backgroundColor:     Colors.blue,
+
 
       body:   Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
 
+
+
         children: [
           Container(
-            height:700,
+            height:MediaQuery.of(context).size.height,
             width: double.maxFinite,
 
-
             child: Card(
-                margin: const EdgeInsets.only(left: 1,top: 30,right: 1,bottom: 1),
+                margin: const EdgeInsets.only(left: 0,top: 0,right: 0,bottom: 0),
                 elevation: 5,
-                shadowColor: Colors.blueAccent,
+                shadowColor: Colors.blue,
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
                     side: BorderSide(width: 5, color: Colors.blueAccent)),
                 color:     Colors.blue,
 
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children:<Widget> [
-                      CircleAvatar(
-                        radius:50,
-                        backgroundImage:
-                        NetworkImage('https://valday.com/img/122841.jpg'),
-                        backgroundColor: Colors.transparent,
+
+                      Container(
+                        padding: new EdgeInsets.only(
+                            top: 100,
+                            left: 0,
+                            right: 0),
+                        child: CircleAvatar(
+                          radius:50,
+                          backgroundImage:
+                          NetworkImage('https://valday.com/img/122841.jpg'),
+                          backgroundColor: Colors.transparent,
+                        ),
                       ),
                       Text(
                         'Союз-Автодор',
@@ -172,16 +181,36 @@ class ParentWidgetPhoneMail extends State<WidgetStateful>  {
 
 
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit),
-        tooltip: 'Edit',
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        hoverColor: Colors.red,
+
+
+////TODO: Кнопка переполучение Данных Когда сервер выключин
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButtonAnimator:  NoScalingAnimation(),
+      floatingActionButton: Container(
+        margin:  const EdgeInsets.only( left: 5,top:5,right: 5,bottom: 50),
+        padding: const EdgeInsets.all(2.0),
+        child: FloatingActionButton (
+          autofocus: true,
+          focusElevation: 5,
+          highlightElevation: 50,
+          onPressed: () {
+          /*  //TODO: при нажатии перезапускаем Получение Данных
+            BI_ChildWidgetError biwidgeterrror=new BI_ChildWidgetError(context: context,logger: logger,key: key);
+            //TODO: метод зарускам когда данных нет но мы запускаем переполучить данные
+            biwidgeterrror.theServeristurnedRereceive();
+
+            logger.i(' CLick FloatingActionButtonLocation  onPressed..   ' );*/
+          },
+          backgroundColor: Colors.blue[300],
+          tooltip: 'Повторное получение !!!',
+          elevation: 15,
+          splashColor: Colors.grey,
+          child:  const Icon(CupertinoIcons.arrow_right,color: Colors.white, size: 29,),
+          shape: RoundedRectangleBorder(
+              borderRadius:  BorderRadius.all(Radius.circular(50.0))
+          ),
+        ),
       ),
-
-
 
 
     );
