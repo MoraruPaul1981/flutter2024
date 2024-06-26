6cea5d139191639950a53cbfec496b5f87b1df20
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-06-26 14:30:23 +0300

    commintprices--07.06.2024------16.47------.zip

--- lib/features/commingprices/blocarchitecture/Businesslayer/codewidgets/BI_ChildWidgetLoginAndPassword.dart
+++ lib/features/commingprices/blocarchitecture/Businesslayer/codewidgets/BI_ChildWidgetLoginAndPassword.dart
@@ -47,37 +47,40 @@ class BI_ChildWidgetLoginAndPassword   implements  IntafaceChildWidgetLoginAndPa
     // TODO: implement callbAckreactiontosomeonewhocameStatus
     try{
       logger.i(' state .. $state ' );
-      Scaffold.of(context).showBottomSheet(
-              (BuildContext context) {
-             /*   //TODO:
-             *     GestureDetector   */
-              return  GestureDetector(
-                    onTap: (){
-                      print("Container clicked");
-                      Navigator.pop(context);
-                    },
-                    child:    Container(
-                        color: Colors.blue[300],//Colors.blue[300] //,redAccent
-                      width: double.infinity,
-                      height: 50,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        child:
-                        Text(
-                          'Логин/Пароль не правильные !!!',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 14.0,
-                            fontFamily: 'Raleway',
-                            color: Colors.black,//TODO Colors.grey.shade600
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                );
-              },
-      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          elevation: 10,
+          behavior: SnackBarBehavior.fixed,
+          shape: const RoundedRectangleBorder(
+            side: BorderSide(color: Colors.white12, width: 1),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20),
+              topLeft: Radius.circular(20),
+            ),
+          ),
+          backgroundColor:Colors.blue[300] ,
+          content: Container(
+            margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 2),
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            color: Colors.blue[300],//Colors.blue[300] //,redAccent
+            width: double.infinity,
+            height: 30,
+            child: Center(
+              child: Text(
+                'Сервер ноправильный ответ !!!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13.0,
+                  fontFamily: 'Raleway',
+                  color: Colors.grey[200],//TODO Colors.grey.shade600
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+          ),
+          duration: const Duration(milliseconds: 1300),
+        ),);
 
      /* //TODO: погда пришел Публинчый ID
      *     */
@@ -248,7 +251,7 @@ class BI_ChildWidgetLoginAndPassword   implements  IntafaceChildWidgetLoginAndPa
       late String  _login  ;
         try{
           //TODO
-             _login=  loginicController.text ;
+             _login=  loginicController.text ?? "" ;
 
           logger.i(' _login .. $_login ' );
 
@@ -273,7 +276,7 @@ class BI_ChildWidgetLoginAndPassword   implements  IntafaceChildWidgetLoginAndPa
     late String  _password  ;
     try{
       //TODO
-      _password=  passwordController.text ;
+      _password=  passwordController.text ?? "" ;
       logger.i(' _password .. $_password ' );
       //TODO error
     }   catch (e, stacktrace) {
