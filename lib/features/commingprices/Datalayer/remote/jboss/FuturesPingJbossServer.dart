ea99373099abf1332ecdcd9c0893d8e94867daab
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-07-10 17:14:38 +0300

    commintprices flutter iyl 2024

--- lib/features/commingprices/Datalayer/remote/jboss/FuturesPingJbossServer.dart
+++ lib/features/commingprices/Datalayer/remote/jboss/FuturesPingJbossServer.dart
@@ -1,5 +1,7 @@
 
 
+
+
 import "dart:async";
 import 'dart:io';
 import 'dart:isolate';
@@ -11,9 +13,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
-import 'package:logger/src/logger.dart';
 
 import '../../../Businesslayer/Use case/Interfaces/InterfaceFuture.dart';
+import '../../../Businesslayer/Use case/errors/Errors.dart';
+import '../../../Businesslayer/Use case/ping/InterfacePing.dart';
+import '../../../Businesslayer/Use case/ping/PingCheck.dart';
 
 
 
@@ -22,11 +26,65 @@ class FuturesPingJbossServer  implements InterfaceFutureResponseJboss {
   //TODO
   //TODO ping Jboss
   @override
-  Future<bool> getResponseJboss({required Uri url }) {
+  Future<bool> getResponseJboss({required Uri url }) async {
     // TODO: implement getResponseJboss
-    print('url....$url');
-    throw UnimplementedError();
+   late bool getStatusJboss  ;
+    try{
+      //TODO Paramerts
+      print('url....$url');
+      InterfacePing pingAyn = PingIpify();
+      bool  PingJboss= await pingAyn.connectedJboss;
+      print('start PingJboss ..${PingJboss}  ' );
+      //TODO получаем State Jboss
+      if (PingJboss==true) {
+        Response   getResponseJboss =   await http.get(url,)
+            .timeout(
+          const Duration(seconds: 20),
+          onTimeout: () {
+            // Time has run out, do what you wanted to do.
+            return  Response(' Timeout Error !!! ', 408); // Request Timeout response status code
+          },
+        )
+            .catchError(
+                (Object error,stacktrace) {
+              print(' get ERROR $error  ');
+            });
+        print('start getResponseJboss ..${getResponseJboss}  ' );
+     /*   //TODO:Статус JBOSS
+     *       */
+         if (getResponseJboss.statusCode==200) {
+           print(' getResponseJboss.statusCode... ${getResponseJboss.statusCode}');
+           getStatusJboss=true;
+
+         }else{
+           print(' getResponseJboss.statusCode... ${getResponseJboss.statusCode}');
+           getStatusJboss=false;
+         }
+
+      }
+
+      //TODO error
+    }   catch (e, stacktrace) {
+      print(' get ERROR $e get stacktrace $stacktrace ');
+      //TODO: Gradle Error
+      Errors errors=Errors();
+      errors.writerError(e: e as Exception, stacktrace: stacktrace as StackTrace );
+    }
+    return  Future<bool>.value(getStatusJboss)       ;
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
   //TODO ping Jboss get Public ID
   @override
   Future<int?> getServergetPublicId({required Uri url,   required String login, required String password}) {
